23d4a3bde46b6754c30238f41aac41d26962b6f1
Andy Davis <davisad@alum.mit.edu>
2020-10-27 11:14:59 -0400

    configure git

--- GenerateDocumentation.sh
+++ GenerateDocumentation.sh
@@ -42,3 +42,13 @@ cd code_documentation
 # Get the current gh-pages branch
 git clone -b documentation https://git@$GH_REPO_REF
 cd $GH_REPO_NAME
+
+##### Configure git.
+# Set the push default to simple i.e. push only the current branch.
+git config --global push.default simple
+# Pretend to be an user called Travis CI.
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+pwd
+ls
