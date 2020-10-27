fb5bdcb2822157d2affe667dbb275666dca59742
Andy Davis <davisad@alum.mit.edu>
2020-10-27 08:59:25 -0400

    added MUQ install if not found

--- cmake/FindMUQ.cmake
+++ cmake/FindMUQ.cmake
@@ -13,8 +13,12 @@ else()
 
   include(ExternalProject)
 
+  # Only enable the parts of MUQ that we really want (i.e., MCMC)
+  set(MUQ_ENABLEGROUP_DEFAULT OFF CACHE BOOL "MUQ Default Compilegroup status")
+  set(MUQ_ENABLEGROUP_UTILITIES_HDF5 ON CACHE BOOL "Enable MUQ HDF5 interface")
+
   ExternalProject_Add(MUQ
     GIT_REPOSITORY https://bitbucket.org/mituq/muq2/src/master/
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external -DMUQ_ENABLEGROUP_DEFAULT=OFF -DMUQ_ENABLEGROUP_UTILITIES_HDF5=ON
   )
 endif()
