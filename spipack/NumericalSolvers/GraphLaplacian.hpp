970c7add8a68efcae8044f743cdb4e35705945a2
Andy Davis <davisad@alum.mit.edu>
2020-10-28 09:10:58 -0400

    Added some documentation for the GraphLaplacian

--- spipack/NumericalSolvers/GraphLaplacian.hpp
+++ spipack/NumericalSolvers/GraphLaplacian.hpp
@@ -6,7 +6,15 @@ namespace NumericalSolvers {
 
 /// Estimate a weighted Laplacian operator using a graph Laplacian
 /**
-  DOCUMENTATION GOES HERE
+  Let \f$\psi\f$ be a probability distribution (with corresponding probability density function). Define the weighted Laplacian operator (given some function \f$H\f$)
+  \f{equation*}{
+    \nabla_{\psi}^2 H = \psi^{-1} \nabla \cdot (\psi \nabla f).
+  \f}
+  This class uses \f$n\f$ samples \f$\{x^{(i)}\}_{i=1}^{n}\f$ such that \f$x^{(i)} \sim \psi\f$ to approximate the action of this operator and to solve the Poisson equation (given the right hand side \f$R\f$)
+  \f{equation*}{
+    \nabla_{\psi}^2 H = R
+  \f}
+  such that \f$\mathbb{E}_{\psi}[H] = 0\f$.
 */
 class GraphLaplacian {
 public:
