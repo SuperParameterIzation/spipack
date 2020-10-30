3c168ef3cc746beab0df3fff55412b525a839782
Andy Davis <davisad@alum.mit.edu>
2020-10-30 14:36:52 -0400

    gtest now uses shared libraries

--- cmake/build/BuildGTEST.cmake
+++ cmake/build/BuildGTEST.cmake
@@ -8,6 +8,7 @@ ExternalProject_Add(GTEST
   -DCMAKE_CXX_FLAGS=-Wall --std=c++11
   -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
   -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+  -DBUILD_SHARED_LIBS=ON
   BUILD_COMMAND make -j5
   INSTALL_COMMAND make -j5 install
 )
@@ -16,5 +17,5 @@ set(GTEST_INCLUDE_DIR
   ${CMAKE_BINARY_DIR}/external/gtest/include)
 
 set(GTEST_LIBRARIES
-  ${CMAKE_BINARY_DIR}/external/gtest/lib/${library_prefix}gmock${static_library_suffix}
-  ${CMAKE_BINARY_DIR}/external/gtest/lib/${library_prefix}gtest${static_library_suffix})
+  ${CMAKE_BINARY_DIR}/external/gtest/lib/${library_prefix}gmock${shared_library_suffix}
+  ${CMAKE_BINARY_DIR}/external/gtest/lib/${library_prefix}gtest${shared_library_suffix})
