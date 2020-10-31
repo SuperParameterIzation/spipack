34c6194c93be2c5a8a0f3e9451963409957b3cb9
Andy Davis <davisad@alum.mit.edu>
2020-10-31 11:27:57 -0400

    debug travis

--- cmake/build/BuildHDF5.cmake
+++ cmake/build/BuildHDF5.cmake
@@ -18,7 +18,7 @@ ExternalProject_Add(
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/hdf5
     URL ${HDF5_EXTERNAL_SOURCE}
     CONFIGURE_COMMAND CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}; ${CMAKE_CURRENT_BINARY_DIR}/external/hdf5/src/HDF5/hdf5-${SPIPACK_INTERNAL_HDF5_VERSION}/configure  CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} ${HDF5_PARALLEL_FLAG} --prefix=${CMAKE_BINARY_DIR}/external/hdf5 --enable-production --disable-examples
-    BUILD_COMMAND make -j5 install &> hdf5-build.txt
+    BUILD_COMMAND make -j5 install > hdf5-build.txt
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND ""
 )
