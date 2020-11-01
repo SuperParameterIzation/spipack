c86eb04e18e55430b7f511c757f28c443f696dcd
Andy Davis <davisad@alum.mit.edu>
2020-10-31 20:08:23 -0400

    debug travis

--- cmake/build/BuildGTEST.cmake
+++ cmake/build/BuildGTEST.cmake
@@ -8,8 +8,8 @@ ExternalProject_Add(GTEST
   -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
   -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
   -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-  -DBUILD_SHARED_LIBS=ON
-  -DBUILD_GMOCK=OFF
+  -DBUILD_SHARED_LIBS=OFF
+  -DBUILD_GMOCK=ON
   BUILD_COMMAND make -j5
   INSTALL_COMMAND make -j5 install
 )
@@ -18,4 +18,4 @@ set(GTEST_INCLUDE_DIR
   ${CMAKE_BINARY_DIR}/external/gtest/include)
 
 set(GTEST_LIBRARIES
-  ${CMAKE_BINARY_DIR}/external/gtest/lib/${library_prefix}gtest${shared_library_suffix})
+  ${CMAKE_BINARY_DIR}/external/gtest/lib/${library_prefix}gmock${shared_library_suffix})
