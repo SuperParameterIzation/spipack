321cd6209cb71e3fb0b5045a8c206a5839b74019
Travis CI User <travis@example.org>
2020-11-02 17:43:30 +0000

    Deploy code docs to GitHub Pages Travis build: 287 [ci skip]

--- documentation/html/search/functions_6.js
+++ documentation/html/search/functions_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['hatkernel',['HatKernel',['../classspi_1_1Tools_1_1HatKernel.html#a132f20e40774fa349c4b6bd3803c3b7a',1,'spi::Tools::HatKernel']]]
+  ['hatkernel',['HatKernel',['../classspi_1_1Tools_1_1HatKernel.html#a132f20e40774fa349c4b6bd3803c3b7a',1,'spi::Tools::HatKernel']]],
+  ['heatmatrixeigenvalues',['HeatMatrixEigenvalues',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#a558441a75364a86399f14dba940659e5',1,'spi::NumericalSolvers::GraphLaplacian']]]
 ];
