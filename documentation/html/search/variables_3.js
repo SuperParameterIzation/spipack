6f7824af372d3f7bda8ff83521e41a05db4e7e00
Travis CI User <travis@example.org>
2020-11-03 16:29:05 +0000

    Deploy code docs to GitHub Pages Travis build: 289 [ci skip]

--- documentation/html/search/variables_3.js
+++ documentation/html/search/variables_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['heatmatrix',['heatMatrix',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#a068763bfd48fbd2f73475dcd7c58c9f4',1,'spi::NumericalSolvers::GraphLaplacian']]]
+  ['eigensolvermaxit',['eigensolverMaxIt',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#a3d087c175d93d9039baa28bf95722ce4',1,'spi::NumericalSolvers::GraphLaplacian::eigensolverMaxIt()'],['../structspi_1_1NumericalSolvers_1_1GraphLaplacian_1_1DefaultParameters.html#acaca58c427c845f346c4d80361e31f87',1,'spi::NumericalSolvers::GraphLaplacian::DefaultParameters::eigensolverMaxIt()']]],
+  ['eigensolvertol',['eigensolverTol',['../classGraphLaplacianTests.html#a83b1661956f028902238700ff56656da',1,'GraphLaplacianTests::eigensolverTol()'],['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#a6e779dac573c2f5bd7736a4dce82cde6',1,'spi::NumericalSolvers::GraphLaplacian::eigensolverTol()'],['../structspi_1_1NumericalSolvers_1_1GraphLaplacian_1_1DefaultParameters.html#a6c7000802534aab542eb74924a23897a',1,'spi::NumericalSolvers::GraphLaplacian::DefaultParameters::eigensolverTol()']]]
 ];
