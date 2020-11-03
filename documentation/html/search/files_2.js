e5dec97b14e0c378bb233097165dddc0635fddf2
Travis CI User <travis@example.org>
2020-11-03 21:11:00 +0000

    Deploy code docs to GitHub Pages Travis build: 296 [ci skip]

--- documentation/html/search/files_2.js
+++ documentation/html/search/files_2.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['graphlaplacian_2ecpp',['GraphLaplacian.cpp',['../GraphLaplacian_8cpp.html',1,'']]],
   ['graphlaplacian_2ehpp',['GraphLaplacian.hpp',['../GraphLaplacian_8hpp.html',1,'']]],
+  ['graphlaplacianexamples_2ecpp',['GraphLaplacianExamples.cpp',['../GraphLaplacianExamples_8cpp.html',1,'']]],
   ['graphlaplaciantests_2ecpp',['GraphLaplacianTests.cpp',['../GraphLaplacianTests_8cpp.html',1,'']]]
 ];
