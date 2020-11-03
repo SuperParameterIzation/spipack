48b27379c8a01812e46a167c4729615258ed3a9b
Andy Davis <davisad@alum.mit.edu>
2020-11-03 13:08:52 -0500

    added numerical solver examples

---
+++ modules/NumericalSolvers/examples/GraphLaplacian/GraphLaplacianExamples.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include <MUQ/Modeling/Distributions/UniformBox.h>
+#include <MUQ/Modeling/Distributions/Gaussian.h>
+
+#include "spipack/NumericalSolvers/GraphLaplacian/GraphLaplacian.hpp"
+
+using namespace muq::Modeling;
+using namespace muq::SamplingAlgorithms;
+using namespace spi::NumericalSolvers;
+
+TEST(GraphLaplacianExamples, HeatMatrixEigenvalues) {
+}
