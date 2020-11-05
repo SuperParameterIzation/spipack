2512852164b2933a1766ba460b9f0cc91eacdfc6
Travis CI User <travis@example.org>
2020-11-05 01:05:49 +0000

    Deploy code docs to GitHub Pages Travis build: 305 [ci skip]

--- documentation/html/search/all_6.js
+++ documentation/html/search/all_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['file',['file',['../namespace__plot__result.html#ac7530501b2338c95fc986d1cc42fdf75',1,'_plot_result']]],
   ['findneighbors',['FindNeighbors',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#a4fe029eec39bf9d5e12241c0a1e42866',1,'spi::NumericalSolvers::GraphLaplacian::FindNeighbors(Eigen::Ref&lt; const Eigen::VectorXd &gt; const &amp;x, double const r2, std::vector&lt; std::pair&lt; std::size_t, double &gt; &gt; &amp;neighbors) const '],['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#acb907bce5a02fd892b0929b1ff216339',1,'spi::NumericalSolvers::GraphLaplacian::FindNeighbors(Eigen::Ref&lt; const Eigen::VectorXd &gt; const &amp;x, std::size_t const k, std::vector&lt; std::pair&lt; std::size_t, double &gt; &gt; &amp;neighbors) const ']]]
 ];
