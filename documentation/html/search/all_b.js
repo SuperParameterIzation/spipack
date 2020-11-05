c7920f421924b9b35bb140dec0627576e361029f
Travis CI User <travis@example.org>
2020-11-05 02:06:01 +0000

    Deploy code docs to GitHub Pages Travis build: 315 [ci skip]

--- documentation/html/search/all_b.js
+++ documentation/html/search/all_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['laplacian',['laplacian',['../classGraphLaplacianTests.html#ac2cb1a391962105f7050892f04a30b6b',1,'GraphLaplacianTests']]]
+  ['laplacian',['laplacian',['../classGraphLaplacianTests.html#ac2cb1a391962105f7050892f04a30b6b',1,'GraphLaplacianTests']]],
+  ['link_2etxt',['link.txt',['../link_8txt.html',1,'']]]
 ];
---
+++ documentation/html/search/files_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['kernel_2ecpp',['Kernel.cpp',['../Kernel_8cpp.html',1,'']]],
+  ['kernel_2ehpp',['Kernel.hpp',['../Kernel_8hpp.html',1,'']]]
+];
