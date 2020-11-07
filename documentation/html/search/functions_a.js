364ac39988e4ca826199d0f62b5cb6ac617a64fc
Travis CI User <travis@example.org>
2020-11-07 21:13:39 +0000

    Deploy code docs to GitHub Pages Travis build: 337 [ci skip]

--- documentation/html/search/functions_a.js
+++ documentation/html/search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['numbandwidthsteps',['NumBandwidthSteps',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#ad0d2f256e9e542aa9685dddeef97904e',1,'spi::NumericalSolvers::GraphLaplacian']]],
   ['numsamples',['NumSamples',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#acb41d0c5d96c7aad4b9dd02d0c1b09de',1,'spi::NumericalSolvers::GraphLaplacian']]]
 ];
