6090d77e219fdfb53329256460663d91e99fe968
Travis CI User <travis@example.org>
2020-11-10 06:01:01 +0000

    Deploy code docs to GitHub Pages Travis build: 365 [ci skip]

--- documentation/html/search/all_f.js
+++ documentation/html/search/all_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['point',['Point',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#ad91ca7aa5bc061a2d0870680be7128bb',1,'spi::NumericalSolvers::GraphLaplacian::Point()'],['../classspi_1_1Tools_1_1NearestNeighbors.html#af998fb02a8a5665aa88949440746a979',1,'spi::Tools::NearestNeighbors::Point()']]]
+  ['point',['Point',['../classspi_1_1Tools_1_1NearestNeighbors.html#af998fb02a8a5665aa88949440746a979',1,'spi::Tools::NearestNeighbors']]]
 ];
--- documentation/html/search/variables_c.js
+++ documentation/html/search/variables_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['representation',['representation',['../classSampleRepresentationTests.html#a45ac82ae4a4801a3f9153d76ba65376e',1,'SampleRepresentationTests']]],
   ['rv',['rv',['../classGraphLaplacianTests.html#aa32419ad2a92d474af4e21008c5a3955',1,'GraphLaplacianTests::rv()'],['../classSampleRepresentationTests.html#a0f8b2f0f4c8be33486cdd12dca5783a5',1,'SampleRepresentationTests::rv()'],['../classNearestNeighborsTests.html#a96257762d588faff7826517b72196a5a',1,'NearestNeighborsTests::rv()']]]
 ];
