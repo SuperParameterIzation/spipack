8223fb775a6839ae308dd21ba13af021e8223cec
Travis CI User <travis@example.org>
2020-11-12 05:59:48 +0000

    Deploy code docs to GitHub Pages Travis build: 412 [ci skip]

--- documentation/html/search/variables_9.js
+++ documentation/html/search/variables_9.js
@@ -1,6 +1,9 @@
 var searchData=
 [
   ['mag',['mag',['../classspi_1_1Tools_1_1BumpKernel.html#a98c0cc081e85738ddfe75aa2d870ca4b',1,'spi::Tools::BumpKernel::mag()'],['../classspi_1_1Tools_1_1ExponentialKernel.html#a2f09eb3860514af8c09d34c60f9ee542',1,'spi::Tools::ExponentialKernel::mag()'],['../classspi_1_1Tools_1_1HatKernel.html#a5b3128fde36bb1963cdfd536b23aa0d2',1,'spi::Tools::HatKernel::mag()']]],
-  ['manifolddim',['manifoldDim',['../classspi_1_1NumericalSolvers_1_1DensityEstimation.html#a35f5d6af79469bf951385b1e0bd8b8ed',1,'spi::NumericalSolvers::DensityEstimation::manifoldDim()'],['../structspi_1_1NumericalSolvers_1_1DensityEstimation_1_1DefaultParameters.html#a21a8130c08ea17ea728471fcbc57d27d',1,'spi::NumericalSolvers::DensityEstimation::DefaultParameters::manifoldDim()']]],
+  ['manifolddim',['manifoldDim',['../classspi_1_1NumericalSolvers_1_1DensityEstimation.html#a8f6f526e5c610a87ca5c6a74ceb77e61',1,'spi::NumericalSolvers::DensityEstimation::manifoldDim()'],['../structspi_1_1NumericalSolvers_1_1DensityEstimation_1_1DefaultParameters.html#a21a8130c08ea17ea728471fcbc57d27d',1,'spi::NumericalSolvers::DensityEstimation::DefaultParameters::manifoldDim()']]],
+  ['markeredgecolor',['markeredgecolor',['../namespace__plot__result.html#a8fd7cf5cbb4839d3c67d0d20e83d422f',1,'_plot_result']]],
+  ['markerfacecolor',['markerfacecolor',['../namespace__plot__result.html#ad37f6d5eaaa6d9d6dc31ebe113bc53e8',1,'_plot_result']]],
+  ['markersize',['markersize',['../namespace__plot__result.html#af88a0e1b0e8af3a04fcbdbac7cd1fe95',1,'_plot_result']]],
   ['maxleaf',['maxLeaf',['../classNearestNeighborsTests.html#aad964fcf45973ff97182f876aa7dac05',1,'NearestNeighborsTests::maxLeaf()'],['../structspi_1_1Tools_1_1NearestNeighbors_1_1DefaultParameters.html#a2bd0c9cec72dd40d70851786e69ea3be',1,'spi::Tools::NearestNeighbors::DefaultParameters::maxLeaf()']]]
 ];
