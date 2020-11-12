027e118ba618d65d48734b20af3cce54435d5721
Travis CI User <travis@example.org>
2020-11-12 15:48:37 +0000

    Deploy code docs to GitHub Pages Travis build: 415 [ci skip]

--- documentation/html/search/files_8.js
+++ documentation/html/search/files_8.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['kernel_2ecpp',['Kernel.cpp',['../Kernel_8cpp.html',1,'']]],
-  ['kernel_2ehpp',['Kernel.hpp',['../Kernel_8hpp.html',1,'']]]
+  ['kernel_2ehpp',['Kernel.hpp',['../Kernel_8hpp.html',1,'']]],
+  ['kolmogorovoperator_2ecpp',['KolmogorovOperator.cpp',['../KolmogorovOperator_8cpp.html',1,'']]],
+  ['kolmogorovoperator_2ehpp',['KolmogorovOperator.hpp',['../KolmogorovOperator_8hpp.html',1,'']]],
+  ['kolmogorovoperatortests_2ecpp',['KolmogorovOperatorTests.cpp',['../KolmogorovOperatorTests_8cpp.html',1,'']]]
 ];
--- documentation/html/search/variables_4.js
+++ documentation/html/search/variables_4.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['eigensolvermaxit',['eigensolverMaxIt',['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#a3d087c175d93d9039baa28bf95722ce4',1,'spi::NumericalSolvers::GraphLaplacian::eigensolverMaxIt()'],['../structspi_1_1NumericalSolvers_1_1GraphLaplacian_1_1DefaultParameters.html#acaca58c427c845f346c4d80361e31f87',1,'spi::NumericalSolvers::GraphLaplacian::DefaultParameters::eigensolverMaxIt()']]],
   ['eigensolvertol',['eigensolverTol',['../classGraphLaplacianTests.html#a83b1661956f028902238700ff56656da',1,'GraphLaplacianTests::eigensolverTol()'],['../classspi_1_1NumericalSolvers_1_1GraphLaplacian.html#a6e779dac573c2f5bd7736a4dce82cde6',1,'spi::NumericalSolvers::GraphLaplacian::eigensolverTol()'],['../structspi_1_1NumericalSolvers_1_1GraphLaplacian_1_1DefaultParameters.html#a6c7000802534aab542eb74924a23897a',1,'spi::NumericalSolvers::GraphLaplacian::DefaultParameters::eigensolverTol()']]],
-  ['eps',['eps',['../classDensityEstimationTests.html#a4dad0a44ec1ac518e4c9279b7c806fa9',1,'DensityEstimationTests']]],
+  ['eps',['eps',['../classDensityEstimationTests.html#a3ac2f41d2bf5053e9744d8a99da32d6f',1,'DensityEstimationTests']]],
   ['expon',['expon',['../classspi_1_1Tools_1_1BumpKernel.html#ae3eb15b5b30650b95aed9bfa497281cb',1,'spi::Tools::BumpKernel::expon()'],['../classspi_1_1Tools_1_1ExponentialKernel.html#aba088c82e3893165c6f63417faa56f69',1,'spi::Tools::ExponentialKernel::expon()']]]
 ];
