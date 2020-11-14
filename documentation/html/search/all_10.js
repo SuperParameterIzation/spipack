fd8bd6dae387eb57179cfd193fa1b4e4d31cc2b2
Travis CI User <travis@example.org>
2020-11-14 20:36:41 +0000

    Deploy code docs to GitHub Pages Travis build: 424 [ci skip]

--- documentation/html/search/all_10.js
+++ documentation/html/search/all_10.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['readme_2etxt',['README.txt',['../README_8txt.html',1,'']]],
-  ['recursivekernelderivativeaverage',['RecursiveKernelDerivativeAverage',['../classspi_1_1NumericalSolvers_1_1SampleRepresentation.html#a14312cf441d3f45e80df7c29c5b8bbeb',1,'spi::NumericalSolvers::SampleRepresentation']]],
-  ['recursivekernelderivativerowaverage',['RecursiveKernelDerivativeRowAverage',['../classspi_1_1NumericalSolvers_1_1SampleRepresentation.html#a80bce07db76f96629985a26a2e4384aa',1,'spi::NumericalSolvers::SampleRepresentation']]],
+  ['recursivekernelderivativeaverage',['RecursiveKernelDerivativeAverage',['../classspi_1_1NumericalSolvers_1_1SampleRepresentation.html#a4accd5e382b68601c1f654a0ffa027d1',1,'spi::NumericalSolvers::SampleRepresentation']]],
+  ['recursivekernelderivativerowaverage',['RecursiveKernelDerivativeRowAverage',['../classspi_1_1NumericalSolvers_1_1SampleRepresentation.html#abe46d342d55aea76daeec09a8306ddaf',1,'spi::NumericalSolvers::SampleRepresentation']]],
   ['representation',['representation',['../classSampleRepresentationTests.html#a45ac82ae4a4801a3f9153d76ba65376e',1,'SampleRepresentationTests']]],
   ['rv',['rv',['../classDensityEstimationTests.html#a8e21b5d085b8250bfa597cccf8a89256',1,'DensityEstimationTests::rv()'],['../classGraphLaplacianTests.html#aa32419ad2a92d474af4e21008c5a3955',1,'GraphLaplacianTests::rv()'],['../classKolmogorovOperatorTests.html#ac84af796f7e3a136f56b11f847001399',1,'KolmogorovOperatorTests::rv()'],['../classSampleRepresentationTests.html#a0f8b2f0f4c8be33486cdd12dca5783a5',1,'SampleRepresentationTests::rv()'],['../classNearestNeighborsTests.html#a96257762d588faff7826517b72196a5a',1,'NearestNeighborsTests::rv()']]]
 ];
