98b3e0e24c91d13f8f91a0246d5ad947d479a95e
Travis CI User <travis@example.org>
2020-11-17 20:37:39 +0000

    Deploy code docs to GitHub Pages Travis build: 464 [ci skip]

--- documentation/html/search/all_6.js
+++ documentation/html/search/all_6.js
@@ -1,6 +1,8 @@
 var searchData=
 [
+  ['f',['f',['../namespace__plot__result.html#acbeab8342bc7f3dcbf2a1546eac654d1',1,'_plot_result']]],
   ['fig',['fig',['../namespace__plot__result.html#a6e39593effbebea8d8ddb94eca917056',1,'_plot_result']]],
   ['findneighbors',['FindNeighbors',['../classspi_1_1Tools_1_1NearestNeighbors.html#ab643fc3f94a39bb0414782233d8371ac',1,'spi::Tools::NearestNeighbors::FindNeighbors(Eigen::Ref&lt; const Eigen::VectorXd &gt; const &amp;point, double const radius2, std::vector&lt; std::pair&lt; std::size_t, double &gt; &gt; &amp;neighbors, std::size_t const &amp;lag=0) const '],['../classspi_1_1Tools_1_1NearestNeighbors.html#a2dae89c063e39e6fa713c3d152dae491',1,'spi::Tools::NearestNeighbors::FindNeighbors(Eigen::Ref&lt; const Eigen::VectorXd &gt; const &amp;point, std::size_t const k, std::vector&lt; std::pair&lt; std::size_t, double &gt; &gt; &amp;neighbors, std::size_t const &amp;lag=0) const ']]],
-  ['format',['format',['../namespace__plot__result.html#af8dc480c7a384a989a9b9d5b26cb4ce6',1,'_plot_result']]]
+  ['format',['format',['../namespace__plot__result.html#af8dc480c7a384a989a9b9d5b26cb4ce6',1,'_plot_result']]],
+  ['ftitle',['ftitle',['../namespace__plot__result.html#a710a8107bdcf36ea821fc65a3c1b4d60',1,'_plot_result']]]
 ];
--- documentation/html/search/variables_5.js
+++ documentation/html/search/variables_5.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['f',['f',['../namespace__plot__result.html#acbeab8342bc7f3dcbf2a1546eac654d1',1,'_plot_result']]],
   ['fig',['fig',['../namespace__plot__result.html#a6e39593effbebea8d8ddb94eca917056',1,'_plot_result']]],
-  ['format',['format',['../namespace__plot__result.html#af8dc480c7a384a989a9b9d5b26cb4ce6',1,'_plot_result']]]
+  ['format',['format',['../namespace__plot__result.html#af8dc480c7a384a989a9b9d5b26cb4ce6',1,'_plot_result']]],
+  ['ftitle',['ftitle',['../namespace__plot__result.html#a710a8107bdcf36ea821fc65a3c1b4d60',1,'_plot_result']]]
 ];
