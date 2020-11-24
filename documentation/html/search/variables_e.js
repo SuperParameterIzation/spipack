1028be85627a61d7ec5934a9202931270b1c110a
Travis CI User <travis@example.org>
2020-11-24 16:26:29 +0000

    Deploy code docs to GitHub Pages Travis build: 470 [ci skip]

--- documentation/html/search/variables_e.js
+++ documentation/html/search/variables_e.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['samples',['samples',['../classspi_1_1NumericalSolvers_1_1BandwidthCost.html#ac926932078c0cb94ea3f5ade09d58996',1,'spi::NumericalSolvers::BandwidthCost::samples()'],['../classspi_1_1NumericalSolvers_1_1SampleRepresentation.html#a61849ff3f4de0526fbf1ad4d89656c5a',1,'spi::NumericalSolvers::SampleRepresentation::samples()'],['../classspi_1_1Tools_1_1NearestNeighbors_1_1Cloud.html#ac8d4b57ce312bbf638557f3f3d468b17',1,'spi::Tools::NearestNeighbors::Cloud::samples()'],['../classspi_1_1Tools_1_1NearestNeighbors.html#a874796f28c5b4e0da528f6cbc14c74ed',1,'spi::Tools::NearestNeighbors::samples()'],['../namespace__plot__result.html#a7890339c67d627c6b7e4334096812504',1,'_plot_result.samples()']]],
-  ['scale',['scale',['../classspi_1_1Tools_1_1BumpKernel.html#a322ca2a644660f9490739cf7262e5666',1,'spi::Tools::BumpKernel::scale()'],['../classspi_1_1Tools_1_1ExponentialKernel.html#ada30e31e99b619cbfad689424988d262',1,'spi::Tools::ExponentialKernel::scale()']]],
+  ['scale',['scale',['../classspi_1_1Tools_1_1BumpKernel.html#a322ca2a644660f9490739cf7262e5666',1,'spi::Tools::BumpKernel::scale()'],['../classspi_1_1Tools_1_1ExponentialKernel.html#ada30e31e99b619cbfad689424988d262',1,'spi::Tools::ExponentialKernel::scale()'],['../namespace__plot__result.html#a473d5e4573c5deaa2c5c9841cdbe97bf',1,'_plot_result.scale()']]],
   ['scatter',['scatter',['../namespace__plot__result.html#a75ee7360d0f8d0d5c17e776a0a0049f8',1,'_plot_result']]],
+  ['sinvlhatsf',['SinvLhatSf',['../namespace__plot__result.html#a58a9ae79cdc0ae813ee7aec2d64081e5',1,'_plot_result']]],
+  ['sinvlhatsftitle',['SinvLhatSftitle',['../namespace__plot__result.html#a366454d9a514b6110e92b9bead729b25',1,'_plot_result']]],
   ['squaredbandwidth',['squaredBandwidth',['../namespace__plot__result.html#af37691d2beca38784dff1964eea67817',1,'_plot_result']]],
   ['std',['std',['../CMakeLists_8txt.html#a74f1ce2eb667ae060cbeba3a048ca2fa',1,'CMakeLists.txt']]]
 ];
--- documentation/html/search/variables_f.js
+++ documentation/html/search/variables_f.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['tailind',['tailind',['../namespace__plot__result.html#a10c4053108b8b2c762f47a82d76c47f9',1,'_plot_result']]],
   ['truedensity',['trueDensity',['../namespace__plot__result.html#a40716c587f1d277e61551d0e6f5ef0e5',1,'_plot_result']]],
   ['truncatekernelmatrix',['truncateKernelMatrix',['../classspi_1_1NumericalSolvers_1_1SampleRepresentation.html#a209f5fe069adbc1811eaa7b2c6420ae9',1,'spi::NumericalSolvers::SampleRepresentation::truncateKernelMatrix()'],['../structspi_1_1NumericalSolvers_1_1SampleRepresentation_1_1DefaultParameters.html#a3e10834c23ca6e176272b7870a5137fd',1,'spi::NumericalSolvers::SampleRepresentation::DefaultParameters::truncateKernelMatrix()']]],
   ['truncationtol',['truncationTol',['../classspi_1_1NumericalSolvers_1_1SampleRepresentation.html#a80e495227a30d0365d2b2b83ce0b94ba',1,'spi::NumericalSolvers::SampleRepresentation']]],
