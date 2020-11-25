1b93b42768659255eaa1d04072423618508a4e0c
Andy Davis <davisad@alum.mit.edu>
2020-11-25 09:23:50 -0500

    updated muq cmake

--- cmake/build/BuildMUQ.cmake
+++ cmake/build/BuildMUQ.cmake
@@ -59,5 +59,6 @@ list(APPEND MUQ_LIBRARIES
   "${CMAKE_BINARY_DIR}/external/muq/lib/${library_prefix}muqUtilities${shared_library_suffix}"
   "${CMAKE_BINARY_DIR}/external/muq/lib/${library_prefix}muqModeling${shared_library_suffix}"
   "${CMAKE_BINARY_DIR}/external/muq/lib/${library_prefix}muqOptimization${shared_library_suffix}"
+  "${CMAKE_BINARY_DIR}/external/muq/lib/${library_prefix}muqApproximation${shared_library_suffix}"
   "${CMAKE_BINARY_DIR}/external/muq/lib/${library_prefix}muqSamplingAlgorithms${shared_library_suffix}"
 )
